dd3df39bc4c434d3f5a613089d0c6d56240e1d11
ssbaek <ssbaek@finaldata.com>
2024-10-24 15:08:07 +0900

    express 추가

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port : number = 3000;
 console.log("Hello, world");
+// express 5 변경점 : https://news.hada.io/topic?id=17320
+app.get("/",(req:Request, res:Response):void => {
+    res.send('Hello, world!');
+});
+
+// 서버 시작
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
