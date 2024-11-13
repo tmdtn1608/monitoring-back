1c326928ce567c601741f567ce3dec6d2bb978ec
BaekSeungSu <tmdtn1608@gmail.com>
2024-11-13 18:27:16 +0900

    실시간

--- src/Routes/ProcessLog.ts
+++ src/Routes/ProcessLog.ts
@@ -5,12 +5,40 @@ import { DB_CLIENT } from '../DBConnector.js';
 const router = Router();
 
 /**
- * 조회
+ * 접속중인 디바이스 x 프로세스 조회
  */
 router.get('/', (req, res) => {
+    let param = req.body;
+    
+    /*
+    SELECT pl.* FROM ProcessLog pl ,
+(SELECT Device, MAX(RegDate) AS last_login
+FROM History h1
+WHERE ActType = 'Client login'
+AND NOT EXISTS (
+    SELECT 1
+    FROM History h2
+    WHERE h2.Device = h1.Device
+    AND h2.ActType = 'Client logout'
+    AND h2.RegDate > h1.RegDate
+)
+GROUP BY Device ) live
+WHERE live.Device = pl.Device AND pl.Device = ''
+    */
 
+    let qb = new StringBuilder();
+    qb.append('SELECT pl.* FROM ProcessLog pl, ')
+    .append(`(SELECT Device, MAX(RegDate) AS last_login FROM History h1 `)
+    .append(`WHERE ActType = 'Client login' AND NOT EXISTS `)
+    .append('(SELECT 1 FROM History h2 WHERE h2.Device = h1.Device ')
+    .append(`AND h2.ActType = 'Client logout' AND h2.RegDate > h1.RegDate)`)
+    .append('GROUP BY Device ) live ')
+    .append('WHERE live.Device = pl.Device AND 1 = 1 ');
+    if (param.device !== undefined && param.device !== null) {
+        qb.append(`pl.Device = '${param.device}'`);
+    }
     DB_CLIENT.GetInstance()
-    .AsyncQuery(`select * from ProcessLog`)
+    .AsyncQuery(qb.toString())
     .then((result) => {
         // const jsonResult = JSON.stringify(result);
         res.json(result);
@@ -20,6 +48,45 @@ router.get('/', (req, res) => {
         res.status(500);
     });
 });
+
+/**
+ * 접속중인 클라이언트 정보 조회
+ */
+router.get("/alive", (req,res) => {
+
+    /*
+
+    SELECT Device, MAX(RegDate) AS last_login
+FROM History h1
+WHERE ActType = 'Client login'
+AND NOT EXISTS (
+    SELECT 1
+    FROM History h2
+    WHERE h2.Device = h1.Device
+    AND h2.ActType = 'Client logout'
+    AND h2.RegDate > h1.RegDate
+    )
+GROUP BY Device
+*/
+    let qb = new StringBuilder();
+    qb.append('SELECT Device, MAX(RegDate) AS last_login FROM History h1 ')
+    .append(`WHERE ActType = 'Client login' AND NOT EXISTS `)
+    .append(`(SELECT 1 FROM History h2 WHERE h2.Device = h1.Device `)
+    .append(`AND h2.ActType = 'Client logout' AND h2.RegDate > h1.RegDate) `)
+    .append('GROUP BY Device');
+
+    DB_CLIENT.GetInstance()
+    .AsyncQuery(qb.toString())
+    .then((result) => {
+        // const jsonResult = JSON.stringify(result);
+        res.json(result);
+    })
+    .catch((error) => {
+        console.error('Error fetching process list:', error);
+        res.status(500);
+    });
+});
+
 
 /**
  * 
@@ -39,7 +106,7 @@ updated_at = CURRENT_TIMESTAMP;
     if (param.process === undefined || param.process === null) {
         res.status(400).send("No actType");
     }
-    
+
     let processJson = JSON.stringify(param.process);
     let qb = new StringBuilder();
     qb.append('INSERT INTO ProcessLog (Device, Process) ')
